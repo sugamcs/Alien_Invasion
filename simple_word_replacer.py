d289053bf9fe64ec3ce9baa8d0ae10c74c760b9f
Sugam Bhargav <78304101+sugamcs@users.noreply.github.com>
2023-06-14 13:46:05 +0530

    Create simple_word_replacer.py

---
+++ simple_word_replacer.py
@@ -0,0 +1,10 @@
+def word_replacer():
+    string = input("Type a sentence: ")
+    word_to_replace = input("What word do you want to replace? ")
+    word_replacement = input("What do you want to replace the word with? ")
+    print(string.replace(word_to_replace,word_replacement))
+
+
+word_replacer()
+
+    
